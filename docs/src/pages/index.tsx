e80c396fbb30cf7276ea7e93f169377b3ddc73c4
Roman Khabarov <roman@globalart.dev>
2025-08-09 09:43:20 +0200

    fix

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,44 +1,6 @@
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-import Layout from '@theme/Layout';
-import clsx from 'clsx';
+import { Redirect } from '@docusaurus/router';
 import type { ReactNode } from 'react';
 
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          NestJS Toolkit
-        </Heading>
-        <p className="hero__subtitle">Essential utilities and decorators for NestJS applications</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/intro">
-            Get Started
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
 export default function Home(): ReactNode {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title="NestJS Toolkit - Essential utilities for NestJS applications"
-      description="A comprehensive toolkit for NestJS applications that provides essential utilities and decorators to simplify common development tasks.">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+  return <Redirect to="/intro" />;
 }
