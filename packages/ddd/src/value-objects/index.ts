4bc42b47eed1e6ea66c61cc962590e49a1a63356
Roman Khabarov <roman@globalart.dev>
2025-09-11 20:57:13 +0200

    fix

--- packages/ddd/src/value-objects/index.ts
+++ packages/ddd/src/value-objects/index.ts
@@ -2,5 +2,6 @@ export * from './boolean.vo';
 export * from './date.vo';
 export * from './id.vo';
 export * from './nanoid.vo';
+export * from './integer.vo';
 export * from './string.vo';
 export * from './value-object';
