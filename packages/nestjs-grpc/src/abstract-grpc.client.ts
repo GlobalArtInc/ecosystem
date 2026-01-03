2c925bc16fe058438e768e0e46dfefcdeda7070f
Roman Khabarov <roman@globalart.dev>
2026-01-03 18:16:57 +0100

    fix

--- packages/nestjs-grpc/src/abstract-grpc.client.ts
+++ packages/nestjs-grpc/src/abstract-grpc.client.ts
@@ -3,12 +3,12 @@ import { firstValueFrom, Observable } from 'rxjs';
 
 type UnwrapObservable<U> = U extends Observable<infer R> ? R : U;
 
-export abstract class AbstractGrpcClient<T extends Record<string, any>> {
+export abstract class AbstractGrpcClient {
 	protected constructor(
 		private readonly client: ClientGrpc,
 	) {}
 
-	public async call<K extends keyof T>(serviceName: string, methodName: string, payload: Parameters<T[K]>[0] = {}): Promise<UnwrapObservable<ReturnType<T[K]>>> {
+	public async call<T extends Record<string, any>, K extends keyof T>(serviceName: string, methodName: string, payload: Parameters<T[K]>[0] = {}): Promise<UnwrapObservable<ReturnType<T[K]>>> {
 		try {
       const service = this.client.getService<T>(serviceName);
 
