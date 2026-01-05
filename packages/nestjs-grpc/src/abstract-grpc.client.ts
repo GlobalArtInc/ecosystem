5ece72fa971e088fae6a14719847227c6dc39a40
Roman Khabarov <roman@globalart.dev>
2026-01-05 11:08:38 +0100

    fix

--- packages/nestjs-grpc/src/abstract-grpc.client.ts
+++ packages/nestjs-grpc/src/abstract-grpc.client.ts
@@ -8,13 +8,30 @@ export abstract class AbstractGrpcClient {
 		private readonly client: ClientGrpc,
 	) {}
 
-	public async call<T extends Record<string, any>>(serviceName: string, methodName: string, payload: Parameters<T[keyof T]>[0] = {}): Promise<UnwrapObservable<ReturnType<T[keyof T]>>> {
-		try {
-      const service = this.client.getService<T>(serviceName);
+	public service<T extends object>(serviceName: string) {
+		return {
+			call: <K extends keyof T>(
+				methodName: K,
+				payload: T[K] extends (...args: infer P) => any ? P[0] : never = {} as any,
+			): Promise<T[K] extends (...args: any) => any ? UnwrapObservable<ReturnType<T[K]>> : never> => {
+				return this.call<T, K>(serviceName, methodName, payload);
+			},
+		};
+	}
 
-      return firstValueFrom(service[methodName](payload));
+	public async call<T extends object, K extends keyof T>(
+		serviceName: string,
+		methodName: K,
+		payload: T[K] extends (...args: infer P) => any ? P[0] : never = {} as any,
+	): Promise<T[K] extends (...args: any) => any ? UnwrapObservable<ReturnType<T[K]>> : never> {
+		try {
+			const service = this.client.getService<T>(serviceName);
+
+			const method = service[methodName] as unknown as (...args: any[]) => Observable<any>;
+
+			return await firstValueFrom(method(payload));
 		} catch (error) {
-			throw error
+			throw error;
 		}
 	}
 }
