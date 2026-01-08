196aa22b947845244f3739a803280afaddf897ae
Roman Khabarov <roman@globalart.dev>
2026-01-08 09:36:53 +0100

    fix

--- packages/nestjs-grpc/src/core/grpc.config.ts
+++ packages/nestjs-grpc/src/core/grpc.config.ts
@@ -2,10 +2,25 @@ import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 import { ReflectionService } from "@grpc/reflection";
 import { HealthImplementation } from "grpc-health-check";
 
-interface GrpcConfig {
+export interface GrpcLoader {
+  keepCase?: boolean;
+  alternateCommentMode?: boolean;
+  longs?: Function;
+  enums?: Function;
+  bytes?: Function;
+  defaults?: boolean;
+  arrays?: boolean;
+  objects?: boolean;
+  oneofs?: boolean;
+  json?: boolean;
+  includeDirs?: string[];
+}
+
+export interface GrpcConfig {
   url: string;
   packageName: string;
   protoPath: string;
+  loader?: GrpcLoader;
 }
 
 export const getGrpcConfig = (options: GrpcConfig): MicroserviceOptions => {
@@ -15,15 +30,16 @@ export const getGrpcConfig = (options: GrpcConfig): MicroserviceOptions => {
       url: options.url,
       package: options.packageName,
       protoPath: options.protoPath,
+      loader: options.loader,
       onLoadPackageDefinition: (pkg, server) => {
         const healthImpl = new HealthImplementation({
-          '': 'UNKNOWN',
+          "": "UNKNOWN",
         });
         healthImpl.addToServer(server);
-        healthImpl.setStatus('', 'SERVING');
+        healthImpl.setStatus("", "SERVING");
 
         new ReflectionService(pkg).addToServer(server);
       },
-    }
-  }
-}
+    },
+  };
+};
--- packages/nestjs-grpc/src/factory/grpc-client.factory.ts
+++ packages/nestjs-grpc/src/factory/grpc-client.factory.ts
@@ -1,36 +1,38 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable } from "@nestjs/common";
 import {
-	ClientGrpc,
-	ClientProxyFactory,
-	Transport
-} from '@nestjs/microservices'
+  ClientGrpc,
+  ClientProxyFactory,
+  Transport,
+} from "@nestjs/microservices";
+import { GrpcLoader } from "../core/grpc.config";
 
 @Injectable()
 export class GrpcClientFactory {
-	private clients = new Map<string, ClientGrpc>()
+  private clients = new Map<string, ClientGrpc>();
 
-	public createClient(options: {
-		package: string
-		protoPath: string
-		url: string
-	}) {
-		return ClientProxyFactory.create({
-			transport: Transport.GRPC,
-			options
-		})
-	}
+  public createClient(options: {
+    package: string;
+    protoPath: string;
+    url: string;
+    loader?: GrpcLoader;
+  }) {
+    return ClientProxyFactory.create({
+      transport: Transport.GRPC,
+      options,
+    });
+  }
 
-	public register(token: string, client: ClientGrpc) {
-		this.clients.set(token, client)
-	}
+  public register(token: string, client: ClientGrpc) {
+    this.clients.set(token, client);
+  }
 
-	public getClient<T extends ClientGrpc = ClientGrpc>(token: string): T {
-		const client = this.clients.get(token)
+  public getClient<T extends ClientGrpc = ClientGrpc>(token: string): T {
+    const client = this.clients.get(token);
 
-		if (!client) {
-			throw new Error(`Grpc client "${token}" not found`);
-		}
+    if (!client) {
+      throw new Error(`Grpc client "${token}" not found`);
+    }
 
-		return client as T
-	}
+    return client as T;
+  }
 }
