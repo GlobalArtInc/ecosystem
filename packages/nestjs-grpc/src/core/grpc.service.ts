05467521f731cc97af3a48bd402ff62887d3d050
Roman Khabarov <roman@globalart.dev>
2026-01-27 23:40:39 +0100

    fix

--- packages/nestjs-grpc/src/core/grpc.service.ts
+++ packages/nestjs-grpc/src/core/grpc.service.ts
@@ -10,21 +10,26 @@ export class GrpcService {
   constructor(private readonly cls: ClsService) {}
 
   public addMetadata(key: string, value: MetadataValue): void {
-    const metadata =
-      this.cls.get<Record<string, MetadataValue>>("GRPC_METADATA") || {};
-    metadata[key] = value;
-    this.cls.set("GRPC_METADATA", metadata);
+    if (this.cls) {
+      const metadata =
+        this.cls.get<Record<string, MetadataValue>>("GRPC_METADATA") || {};
+      metadata[key] = value;
+      this.cls.set("GRPC_METADATA", metadata);
+    }
   }
 
   public getMetadata(): Map<string, MetadataValue> {
-    const grpcMetadata =
-      this.cls.get<Map<string, MetadataValue>>("GRPC_METADATA");
-    if (grpcMetadata) {
-      return new Map(
-        Object.entries(grpcMetadata).map(([key, value]) => [key, value])
-      );
-    } else {
-      return new Map<string, MetadataValue>();
+    if (this.cls) {
+      const grpcMetadata =
+        this.cls.get<Map<string, MetadataValue>>("GRPC_METADATA");
+      if (grpcMetadata) {
+        return new Map(
+          Object.entries(grpcMetadata).map(([key, value]) => [key, value])
+        );
+      } else {
+        return new Map<string, MetadataValue>();
+      }
     }
+    return new Map<string, MetadataValue>();
   }
 }
