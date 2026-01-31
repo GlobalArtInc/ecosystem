25991608f8fe0a66d4838008d27379cb760a5ff4
Roman Khabarov <roman@globalart.dev>
2026-01-31 19:50:34 +0100

    fix

--- packages/nestjs-unit-of-work/src/core/index.ts
+++ packages/nestjs-unit-of-work/src/core/index.ts
@@ -1,3 +1,2 @@
 export { UnitOfWorkManager } from './uow.service';
 export { UnitOfWorkModule } from './uow.module';
-export { IsolationLevel } from '../enums/uow.enums';
--- packages/nestjs-unit-of-work/src/index.ts
+++ packages/nestjs-unit-of-work/src/index.ts
@@ -1,4 +1,3 @@
 export * from './core';
 export * from './decorators';
 export * from './enums';
-export * from './interceptors';
