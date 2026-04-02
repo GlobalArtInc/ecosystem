70ee5de72b13033c4328a7720bebfec1c0b247e7
Roman Khabarov <roman@globalart.dev>
2026-04-02 13:35:53 +0200

    fix: handle errors in observable result processing in TemporalExplorer

--- packages/nestjs-temporal/src/temporal.explorer.ts
+++ packages/nestjs-temporal/src/temporal.explorer.ts
@@ -286,7 +286,7 @@ export class TemporalExplorer
                 return isObservable(result)
                   ? await lastValueFrom(result)
                   : await result;
-              } finally {
+              } catch {
                 clearInterval(interval);
               }
             };
